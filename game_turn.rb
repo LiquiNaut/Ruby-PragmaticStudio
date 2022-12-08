acf8fa49cd77777aa3addbd224ad6a2511c5a0d4
boetfly <boetfly@gmail.com>
2022-12-09 00:09:55 +0100

    created and learned to use STRUCT, created tests for it in treasure_trove_spec.rb

--- game_turn.rb
+++ game_turn.rb
@@ -2,6 +2,7 @@
 
 require_relative 'player'
 require_relative 'die'
+require_relative 'treasure_trove'
 
 module GameTurn
   def self.take_turn(player)
@@ -24,5 +25,8 @@ module GameTurn
     else
       player.w00t
     end
+
+    treasure = TreasureTrove.random
+    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
   end
 end
---
+++ treasure_trove.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Treasure = Struct.new(:name, :points)
+
+module TreasureTrove
+  TREASURES = [
+    Treasure.new(:pie, 5),
+    Treasure.new(:bottle, 25),
+    Treasure.new(:hammer, 50),
+    Treasure.new(:skillet, 100),
+    Treasure.new(:broomstick, 200),
+    Treasure.new(:crowbar, 400)
+  ].freeze
+
+  def self.random
+    TREASURES.sample
+  end
+end
