fd80e4549fb1a7003318791ee900ad83178b2791
boetfly <boetfly@gmail.com>
2022-12-13 00:37:38 +0100

    created tests for sum of all treasures points

--- player_spec.rb
+++ player_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'spec_helper'
+require_relative 'treasure_trove'
 
 describe Player do
 
@@ -72,5 +73,21 @@ describe Player do
     it "is sorted by decreasing score" do
       @players.sort.should == [@player3, @player2, @player1]
     end
+  end
+
+  it "computes points as the sum of all treasure points" do
+    @player.points.should == 0
+
+    @player.found_treasure(Treasure.new(:hammer, 50))
+
+    @player.points.should == 50
+
+    @player.found_treasure(Treasure.new(:crowbar, 400))
+
+    @player.points.should == 450
+
+    @player.found_treasure(Treasure.new(:hammer, 50))
+
+    @player.points.should == 500
   end
 end
