108a9e2ed44b8c59f8d40e503a98e71be6a02883
boetfly <boetfly@gmail.com>
2022-12-13 00:52:32 +0100

    created 2 new methods: points and found_treasure

--- player.rb
+++ player.rb
@@ -7,6 +7,17 @@ class Player
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
+    @found_treasures = Hash.new(0)
+  end
+
+  def points
+    @found_treasures.values.reduce(0, :+)
+  end
+
+  def found_treasure(treasure)
+    @found_treasures[treasure.name] += treasure.points
+    puts "#{name} found a #{treasure.name} worth #{treasure.points} points."
+    puts "#{name}'s treasures: #{@found_treasures}"
   end
 
   def strong?
