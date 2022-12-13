03b1d228f0bab7fdf68a11925e6290e417900826
boetfly <boetfly@gmail.com>
2022-12-13 01:05:02 +0100

    updated take_turn method

--- game_turn.rb
+++ game_turn.rb
@@ -27,6 +27,6 @@ module GameTurn
     end
 
     treasure = TreasureTrove.random
-    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
+    player.found_treasure(treasure)
   end
 end
