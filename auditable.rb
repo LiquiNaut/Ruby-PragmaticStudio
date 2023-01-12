ae907423823f3989bb3274515b7ffa7aa8ee7d48
boetfly <boetfly@gmail.com>
2023-01-12 14:52:56 +0100

    Audit Dice Rolls

---
+++ auditable.rb
@@ -0,0 +1,6 @@
+module Auditable
+  def audit
+    puts "Rolled a #{self.number} (#{self.class})"
+  end
+end
+
--- game_turn.rb
+++ game_turn.rb
@@ -3,6 +3,7 @@
 require_relative 'player'
 require_relative 'die'
 require_relative 'treasure_trove'
+require_relative 'loaded_die'
 
 module GameTurn
   def self.take_turn(player)
